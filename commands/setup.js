b36673b3befc9d0c038b299248fe391f59c66dbb
ultraultimated <neelp1543@gmail.com>
2021-03-07 01:20:11 -0500

    added jenkins installation script for config-srv #2

--- commands/setup.js
+++ commands/setup.js
@@ -30,8 +30,8 @@ exports.handler = async argv => {
 };
 
 async function run(privateKey) {
-	
-	let filePath = '/bakerx/pipeline/playbook.yml';
+
+    let filePath = '/bakerx/pipeline/playbook.yml';
     let inventoryPath = '/bakerx/pipeline/inventory.ini';
 
     console.log(chalk.greenBright('Installing configuration server!'));
@@ -40,11 +40,14 @@ async function run(privateKey) {
     let result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-	console.log(chalk.blueBright('Running init script...'));
+    console.log(chalk.blueBright('Running init script...'));
     result = sshSync('/bakerx/pipeline/server-init.sh', 'vagrant@192.168.33.20');
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
-	
-	console.log(chalk.blueBright('Running ansible script'));
+
+    console.log(chalk.blueBright('Running ansible script to install jenkins...'));
     result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath}`, 'vagrant@192.168.33.20');
     if( result.error ) { process.exit( result.status ); }
+
+
+
 }
