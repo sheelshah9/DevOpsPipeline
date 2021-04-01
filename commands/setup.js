c39a43761903721124adca0b5e3ead4fcb1d8d54
Sheel Jayesh Shah <sshah28@wolftech.ad.ncsu.edu>
2021-03-31 22:26:44 -0400

    added cleanup stage for iTrust job #19 #20

--- commands/setup.js
+++ commands/setup.js
@@ -48,7 +48,7 @@ async function run(privateKey, GH_USER, GH_PASS) {
     console.log(chalk.greenBright('Installing configuration server!'));
 
     console.log(chalk.blueBright('Provisioning configuration server...'));
-    let result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    let result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --memory 4096 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Running init script...'));
