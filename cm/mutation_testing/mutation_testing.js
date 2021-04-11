ab6cc5586f8773360666e796523f19e38ee81707
nnparik2 <nnparik2@ncsu.edu>
2021-04-11 10:27:09 -0400

    refactored the code for fuzzing

--- cm/mutation_testing/mutation_testing.js
+++ cm/mutation_testing/mutation_testing.js
@@ -1,7 +1,6 @@
 const Random = require('random-js');
 const fs = require('fs');
 const path = require('path');
-const stackTrace = require('stacktrace-parser');
 const spawnSync = require('child_process').spawnSync;
 const rimraf = require("rimraf")
 
@@ -23,17 +22,17 @@ class mutater {
     }
 
     static mutateStr(str) {
-        var array = str.split("\n");
+        let array = str.split("\n");
         let n = array.length
-        let tenPercent = n / 5
+        let tenPercent = n / 10
         let rSet = new Set()
-        while(rSet.size < tenPercent) {
+        while (rSet.size < tenPercent) {
             rSet.add(mutater._random.integer(0, array.length - 1))
         }
-        var cp = [];
-        for (var i = 0; i < array.length; i++){
+        let cp = [];
+        for (var i = 0; i < array.length; i++) {
             let elem;
-            if(rSet.has(i)) {
+            if (rSet.has(i)) {
 
                 elem = array[i]
                 if (!isImportOrPackage(elem.trim())) {
@@ -59,8 +58,7 @@ class mutater {
                     }
 
                 }
-            }
-            else {
+            } else {
                 elem = array[i];
             }
             cp.push(elem)
@@ -85,19 +83,17 @@ function isImportOrPackage(str) {
     return false;
 }
 
-function mutationTesting(paths,iterations, path_mutation_dir)
-{
+function mutationTesting(paths, iterations, path_mutation_dir) {
 
     var testResults = [];
-    var tmpdirpath = path.join(srcDirectory,'tmp');
+    var tmpdirpath = path.join(srcDirectory, 'tmp');
     passedTests = 0;
-    
+
     if (!fs.existsSync(path_mutation_dir)) {
         fs.mkdirSync(path_mutation_dir);
-    }
-    else {
-    	rimraf.sync(path_mutation_dir)
-	    fs.mkdirSync(path_mutation_dir);
+    } else {
+        rimraf.sync(path_mutation_dir)
+        fs.mkdirSync(path_mutation_dir);
     }
 
 
@@ -107,19 +103,19 @@ function mutationTesting(paths,iterations, path_mutation_dir)
         if (!fs.existsSync(tmpdirpath)) {
             fs.mkdirSync(tmpdirpath);
         }
-	let iterDirPath = path.join(path_mutation_dir, iter.toString())
-	if (!fs.existsSync(iterDirPath)){
-	    fs.mkdirSync(iterDirPath);
-	}
-        var modfilescache = {};        
-        var filepath = paths[mutater._random.integer(0,paths.length-1)];
-        var filesplit = filepath.split(path.sep);
-        var filename = filesplit[filesplit.length-1];
-        var src = fs.readFileSync(filepath,'utf-8');
-        var dstpath = path.join(tmpdirpath, filename);
-        
+        let iterDirPath = path.join(path_mutation_dir, iter.toString())
+        if (!fs.existsSync(iterDirPath)) {
+            fs.mkdirSync(iterDirPath);
+        }
+        let modfilescache = {};
+        let filepath = paths[mutater._random.integer(0, paths.length - 1)];
+        let filesplit = filepath.split(path.sep);
+        let filename = filesplit[filesplit.length - 1];
+        let src = fs.readFileSync(filepath, 'utf-8');
+        let dstpath = path.join(tmpdirpath, filename);
+
         //If file already mutated, skip the file
-        if(modfilescache.hasOwnProperty(dstpath))
+        if (modfilescache.hasOwnProperty(dstpath))
             continue;
 
         //Store the path to modified files in a dict
@@ -128,7 +124,7 @@ function mutationTesting(paths,iterations, path_mutation_dir)
         //Copy the original file to tmp folder
         try {
             fs.copyFileSync(filepath, dstpath)
-        }catch (err) {
+        } catch (err) {
             throw err;
         }
 
@@ -145,94 +141,93 @@ function mutationTesting(paths,iterations, path_mutation_dir)
         });
 
 
-        try
-        {
+        try {
             //Run the test suite
-            var output = spawnSync(`cd ${srcDirectory} && mvn clean test verify org.apache.maven.plugins:maven-checkstyle-plugin:3.1.0:checkstyle`, { encoding: 'utf-8', stdio: 'pipe' , shell: true });
+            let output = spawnSync(`cd ${srcDirectory} && mvn clean test verify org.apache.maven.plugins:maven-checkstyle-plugin:3.1.0:checkstyle`, {
+                encoding: 'utf-8',
+                stdio: 'pipe',
+                shell: true
+            });
             //Push the test results to the array
-            testResults.push( {input: iter, stack: output.stdout} );
-        }
-        catch(e)
-        {
+            testResults.push({input: iter, stack: output.stdout});
+        } catch (e) {
             // If build fails restart iteration
             console.log("Build error: Restarting iteration\n" + e);
             iter--;
         }
 
         //Copy the original files back to the working directory
-        revertfiles(modfilescache,tmpdirpath);
+        revertfiles(modfilescache, tmpdirpath);
     }
 
     failedTests = {};
     // Get failed tests in each iteration
     let countFailed = 0
 
-    for( var i =0; i < testResults.length; i++ )
-    {
+    for (var i = 0; i < testResults.length; i++) {
         let flag = false
-        var failed = testResults[i];
-        var msg = failed.stack.split("\n");
+        let failed = testResults[i];
+        let msg = failed.stack.split("\n");
 
-	    let mainName = "FAILURE!"
-        msg.filter(function(line) {
-            if(line.includes("<<< FAILURE!") || line.includes("<<< ERROR!") || line.includes("FAILURE!") || line.includes("ERROR!") ){
+        let mainName = "FAILURE!"
+        msg.filter(function (line) {
+            if (line.includes("<<< FAILURE!") || line.includes("<<< ERROR!") || line.includes("FAILURE!") || line.includes("ERROR!")) {
                 var temp = line.split(" ");
-                var test =  temp[1].substring(temp[1].indexOf("(") + 1,temp[1].indexOf(")")) + "." + temp[1].split("(")[0];
-		let currDir = temp[temp.length - 1]
-	    if(currDir !=='FAILURE!' && currDir !== 'ERROR!')
-		    mainName = currDir 
-	    test = mainName + test
-        if (test in failedTests){
-            let arr = failedTests[test]
-			arr.push(failed.input)
-			failedTests[test] = arr;
-		}
-        else
-            failedTests[test] = [failed.input]
+                var test = temp[1].substring(temp[1].indexOf("(") + 1, temp[1].indexOf(")")) + "." + temp[1].split("(")[0];
+                let currDir = temp[temp.length - 1]
+                if (currDir !== 'FAILURE!' && currDir !== 'ERROR!')
+                    mainName = currDir
+                test = mainName + test
+                if (test in failedTests) {
+                    let arr = failedTests[test]
+                    arr.push(failed.input)
+                    failedTests[test] = arr;
+                } else
+                    failedTests[test] = [failed.input]
 
-        if(!flag) {
-            countFailed += 1
-            flag = true
-        }
-        }
+                if (!flag) {
+                    countFailed += 1
+                    flag = true
+                }
+            }
         });
     }
 
     // Create items array
-    var items = Object.keys(failedTests).map(function(key) {
+    let items = Object.keys(failedTests).map(function (key) {
         return [key, failedTests[key]];
     });
 
     // Sort the array based on the second element
-    items.sort(function(first, second) {
+    items.sort(function (first, second) {
         return second[1] - first[1];
     });
 
     //Print test results
-	let percentage = (countFailed / iterations ) * 100
+    let percentage = (countFailed / iterations) * 100
     console.log(`Overall mutation coverage: ${countFailed}/${iterations} (${percentage}%) mutations caught by the test suite.`)
-	console.log("Useful tests" + "\n" + "============")
+    console.log("Useful tests" + "\n" + "============")
     items.forEach(item => {
-	    let splitted = item[0].split(".")
-        if (splitted[splitted.length - 1].substring(0, 4)  === "test") {
+        let splitted = item[0].split(".")
+        if (splitted[splitted.length - 1].substring(0, 4) === "test") {
             console.log(`${item[1].length}/${iterations} ` + item[0])
-		let failedIter = item[1]
-		for(var i = 0; i < failedIter.length; i++) {
-			let filenames = fs.readdirSync("/home/vagrant/mutation_dir" + "/" + failedIter[i].toString())
-			console.log(`\t- /home/vagrant/mutation_dir/${failedIter[i]}/${filenames[0]}`)
-		}
+            let failedIter = item[1]
+            for (var i = 0; i < failedIter.length; i++) {
+                let filenames = fs.readdirSync("/home/vagrant/mutation_dir" + "/" + failedIter[i].toString())
+                console.log(`\t- /home/vagrant/mutation_dir/${failedIter[i]}/${filenames[0]}`)
+            }
         }
     });
 }
 
 
-function revertfiles(modfilescache, dirpath){
+function revertfiles(modfilescache, dirpath) {
     if (fs.existsSync(dirpath)) {
         fs.readdirSync(dirpath).forEach((file, index) => {
             const curPath = path.join(dirpath, file);
             try {
                 fs.copyFileSync(curPath, modfilescache[curPath])
-            }catch (err) {
+            } catch (err) {
                 throw err
             }
             fs.unlinkSync(curPath);
@@ -241,38 +236,38 @@ function revertfiles(modfilescache, dirpath){
     }
 }
 
-function getsourcepath(){
-    var srcdirpath = path.join(srcDirectory,'src','main','java');
+function getsourcepath() {
+    var srcdirpath = path.join(srcDirectory, 'src', 'main', 'java');
     var srcpaths = [];
     //Get a list of source files
     traverseDir(srcdirpath, srcpaths);
     return srcpaths;
 }
 
-function traverseDir(dir,srcpaths) {
+function traverseDir(dir, srcpaths) {
     fs.readdirSync(dir).forEach(file => {
         let fullPath = path.join(dir, file);
         if (fs.lstatSync(fullPath).isDirectory()) {
-            traverseDir(fullPath , srcpaths);
+            traverseDir(fullPath, srcpaths);
         } else {
             let path_split = fullPath.split(".");
-            if(path_split[path_split.length-1] == 'java')
+            if (path_split[path_split.length - 1] == 'java')
                 srcpaths.push(fullPath);
         }
     });
 }
 
-function main(){
+function main() {
     mutater.seed(11);
     paths = getsourcepath();
     path_mutation_dir = "/home/vagrant/mutation_dir"
-    mutationTesting(paths,iterations, path_mutation_dir);
+    mutationTesting(paths, iterations, path_mutation_dir);
 }
 
 
 exports.mutationTesting = mutationTesting;
 exports.mutater = mutater;
 
-if (require.main === module){
+if (require.main === module) {
     main();
 }
