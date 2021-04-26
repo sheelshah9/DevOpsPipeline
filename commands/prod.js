8b5f8fdbaaec0f6482d620f11c1b279619871682
bpatel24 <bpatel24@ncsu.edu>
2021-04-25 22:48:31 -0400

    added code to create inventory.ini #29

--- commands/prod.js
+++ commands/prod.js
@@ -6,85 +6,136 @@ const fs = require('fs')
 const scpSync = require('../lib/scp');
 
 let config = {};
-// Retrieve our api token from the environment variables.
+
 config.token = '';
-
-console.log(chalk.green(`Your token is: ${config.token.substring(0,4)}...`));
-
-// Configure our headers to use our token when making REST api requests.
+var count= 0;
 
 class DigitalOceanProvider
 {
     async createDroplet (dropletName, region, imageName )
     {
-        if( dropletName == "" || region == "" || imageName == "" )
+                if( dropletName == "" || region == "" || imageName == "" )
+                {
+                        console.log( chalk.red("You must provide non-empty parameters for createDroplet!") );
+                        return;
+                }
+
+                var data = 
+                JSON.stringify({
+                        "name":dropletName,
+                        "region":region,
+                        "size":"s-1vcpu-1gb",
+                        "image":imageName,
+                        "ssh_keys": [process.env.FINGERPRINT ],
+                        "backups":false,
+                        "ipv6":false,
+                        "user_data":null,
+                        "private_networking":null
+                });
+
+                console.log("Attempting to create: "+ JSON.stringify(data) );
+           
+		 var headers =
+                {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + config.token
+                };
+
+	       let response = await got.post("https://api.digitalocean.com/v2/droplets",
+                {
+                        headers:headers,
+                        body: data,
+                        json: true
+                }).catch( err =>
+                        console.error(chalk.red(`createDroplet: ${err}`)) );
+               
+
+        	if( !response ) return;
+
+        	console.log(response.statusCode);
+        	let droplet = response.body.droplet; // JSON.parse( response.body ).droplet;
+
+        	if(response.statusCode == 202)
+        	{
+         	   console.log(chalk.green(`Created droplet id ${droplet.id}`));
+        	}
+        
+        	await this.dropletInfo(dropletName, droplet.id);
+    }
+
+    async dropletInfo (name, id)
+    {
+	var ip;
+        if( typeof id != "number" )
         {
-            console.log( chalk.red("You must provide non-empty parameters for createDroplet!") );
+            console.log( chalk.red("You must provide an integer id for your droplet!") );
             return;
         }
-
-        var data =
-            {
-                "name": dropletName,
-                "region":region,
-                "size":"s-1vcpu-1gb",
-                "image":imageName,
-                "ssh_keys": [process.env.FINGERPRINT], 
-                "backups":false,
-                "ipv6":false,
-                "user_data":null,
-                "private_networking":null
-            };
-
-        console.log("Attempting to create: "+ JSON.stringify(data) );
-
         var headers =
-    	    {
-        	'Content-Type':'application/json',
-        	Authorization: 'Bearer ' + config.token
-	    };
+	{
+                'Content-Type':'application/json',
+                Authorization: 'Bearer ' + config.token
+        };
 
-	let response = await got.post("https://api.digitalocean.com/v2/droplets",
-            {
-                headers:headers,
-                json: data
-            }).catch( err =>
-            console.error(chalk.red(`createDroplet: ${err}`))
-        );
+     
+        var ping = setInterval(async function(){
+			let response = await got('https://api.digitalocean.com/v2/droplets/' + id, { headers: headers, responseType: 'json' })
+			.catch(err => console.error(`dropletinfo ${err}`));
+                       
+			if( !response ) return;
 
-        if( !response ) return;
+			if( JSON.parse(response.body).droplet )
+			{
+				let droplet = JSON.parse(response.body).droplet;
 
-        console.log(response.statusCode);
-        let droplet = JSON.parse( response.body ).droplet;
+				if(droplet.status == "active"){
+					console.log(`${name} VM active`);
+					// Print out IP address
+					ip = droplet.networks.v4[1].ip_address
+					console.log(`IP Address: ${ip}`);
 
-        if(response.statusCode == 202)
-        {
-            console.log(chalk.green(`Created droplet id ${droplet.id}`));
-        }
-    } 
+					if(name == "checkbox"){
+						writeFile("checkbox", ip);
+					}else if(name == "iTrust"){
+						writeFile("iTrust", ip);
+					}else if(name == "monitoring"){
+						writeFile("monitoring", ip);
+					}
+					clearInterval(ping);
+				}
+			}
 
+		}, 1000);
+    }
 };
+
+
 async function provision(argv)
 {
 
-     config.token = argv.API_TOKEN;
+       fs.writeFile('inventory.ini', '', function (err) {
+		if (err) throw err;
+		console.log('Reset Inventory File');
+	})
 
-     if( !config.token )
+       config.token = argv.API_TOKEN;
+	if( !config.token )
 	{
-    		console.log(chalk.red`{red.bold pass Digital ocean API_TOKEN as an argument. See README or type --help for more information!}`);    		
+    		console.log(chalk.red`{red.bold pass Digital ocean API_TOKEN as an argument. See README or type --help for more information!}`);
     		process.exit(1);
 	}
-
+    
 	let client = new DigitalOceanProvider();
-
-     // #3 Create an droplet with the specified name, region, and image
-     var names = ["checkbox", "iTrust", "monitoring"]
-     for (var i = 0; i < names.length; i++) {
-        var name = names[i]
-        var region = "nyc1"; // Fill one in from #1
-        var image = "debian-10-x64"; // Fill one in from #2
-        await client.createDroplet(name, region, image);
-     }
+    
+	// Create an droplet with the specified name, region, and image
+	var names = ["checkbox", "iTrust", "monitoring"]
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i]
+		var region = "nyc3"; // Fill one in from #1
+		var image = "ubuntu-16-04-x32"; // Fill one in from #2
+		await client.createDroplet(name, region, image);
+	}
+    
 }
 
 exports.command = '$0 prod <name>';
@@ -99,10 +150,24 @@ exports.builder = yargs => {
     });
 };
 
+function writeFile(name, ip){
+	var inventory = "[" + name + "]\n" + ip + "   ansible_ssh_private_key_file=/home/vagrant/.ssh/private_key    ansible_user=root\n\n["+ name+ ":vars]\nansible_ssh_common_args='-o StrictHostKeyChecking=no'\n\n";
+
+	fs.appendFile('inventory.ini', inventory, function (err) {
+		if (err) throw err;
+		console.log(`Added IP Address for ${name} to inventory file`);
+		count += 1;
+
+		if (count == 3){
+                         var temp =  scpSync("./inventory.ini", "vagrant@192.168.33.20:/bakerx/cm/");
+        	}
+	})
+}
+
 
 // Run workshop code...
 exports.handler = async argv => {
     (async () => {
-	await provision(argv);	
+	await provision(argv);
     })();
 };
